bd375859e45931e9b3c27658b35372b0d3d68f51
Sean P. Myrick V19.1.7.2 <65933340+seanpm2001@users.noreply.github.com>
2021-09-20 14:37:02 -0700

    makefile.mk
    The makefile for this directory set.

---
+++ 0/makefile.mk
@@ -0,0 +1,11 @@
+# Start of script
+# This makefile should be ran during the installation process automatically.
+rm -rf /0/~EXCLUDE
+rm -rf /0/README.txt
+wait 2
+rm -rf /0/makefile.mk
+# File info
+# File version: 1 (Monday, 2021 September 20th at 2:37 pm)
+# File type: Makefile (*.mk)
+# Line count (including blank lines and compiler line): 12
+# End of script
